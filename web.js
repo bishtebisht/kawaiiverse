e5d2e3dd50fb9ab7bf7eaa94209d1005f6ec199e
Bhoomi Bisht <bhoomibisht007@gmail.com>
2025-10-03 20:21:45 +0530

    Update web.js

--- web.js
+++ web.js
@@ -28,64 +28,7 @@ arrowsRight.forEach((arrowRight,i)=>{
     console.log(Math.floor(window.innerWidth/200));
   
 });
-// const nextBtn=document.querySelector(".nav-btn.right");
-// const prevBtn=document.querySelector(".nav-btn-left");
-// const spotlights=document.querySelectorAll(".spotlight");
 
-// nextBtn.forEach((nextBtn,i)=>{
-//     const items=spotlights[i].querySelectorAll("img").length; 
-//     let Counter=0;
-//     nextBtn.addEventListener("click",()=>{
-//     const ratio=Math.floor(window.innerWidth/800);
-//         Counter++;
-//         if(items-(1+Counter)+(2-ratio)>=0){
-//             spotlights[i].style.transform=`translateX(${
-//                 spotlights[i].computedStyleMap().get("transform")[0].x.value
-//             -850}px)`;
-//         }else{
-//             spotlights[i].style.transform="translateX(0)";
-//             Counter=0;
-//         }
-//     });
-//     prevBtn[i].addEventListener("click", () => {
-//         if (Counter > 0) {
-//             Counter--;
-//             spotlights[i].style.transform = `translateX(${
-//                 spotlights[i].computedStyleMap().get("transform")[0].x.value + 220
-//             }px)`;
-//         }
-//     }); 
-//     console.log(Math.floor(window.innerWidth/800));
-  
-// });
-// const animeLists = document.querySelectorAll(".anime-list");
-// const arrowsLeft = document.querySelectorAll(".arrow-left");
-// const arrowsRight = document.querySelectorAll(".arrow-right");
-
-// animeLists.forEach((animeList, i) => {
-//     const items = animeList.querySelectorAll(".anime-item"); 
-//     const totalItems = items.length;
-//     let currentIndex = 0;
-
-//     // Right arrow
-//     arrowsRight[i].addEventListener("click", () => {
-//         currentIndex++;
-//         if (currentIndex >= totalItems - 5) {  
-//             // "-4" so it shows 5 items max at once
-//             currentIndex = 0;  
-//         }
-//         animeList.style.transform = `translateX(-${currentIndex * 220}px)`; 
-//     });
-
-//     // Left arrow
-//     arrowsLeft[i].addEventListener("click", () => {
-//         currentIndex--;
-//         if (currentIndex < 0) {
-//             currentIndex = totalItems - 6; // back to last "page"
-//         }
-//         animeList.style.transform = `translateX(-${currentIndex * 220}px)`; 
-//     });
-// });
 
 const spotlightItem = document.querySelector(".spotlight-item");
 const spotlights = document.querySelectorAll(".spotlight");
@@ -111,26 +54,3 @@ prevBtn.addEventListener("click", () => {
     spotlightItem.style.transform = `translateX(-${currentIndex * 104.08}%)`;
 });
 
-// const animeList = document.querySelector(".anime-list");
-// const animeItem= document.querySelectorAll(".anime-item");
-// const arrowsLeft = document.querySelector(".arror-left");
-// const arrowsRight = document.querySelector(".arroe-right");
-
-// let counter = 0;
-// const totalanime = animeItem.length;
-
-// arrowsRight.addEventListener("click", () => {
-//     counter++;
-//     if (counter >= totalanime) {
-//         counter = 0;
-//     }
-//     animeList.style.transform = `translateX(-${counter * 104.05}%)`;
-// });
-
-// arrowsLeft.addEventListener("click", () => {
-//     counter--;
-//     if (counter < 0) {
-//         counter = totalSpotlights - 1;
-//     }
-//     animeList.style.transform = `translateX(-${counter * 104.05}%)`;
-// });
